4cb1da9434fe01e531b9ce2c1a768c7a647f06e8
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2018-05-26 15:02:58 +0530

    Sending a http response after configuring the request routes.

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import express from 'express';
 const PORT = 3000;
 const server = express();
 
+server.get('/',(req,res) => {
+res.send('My first route using express js');
+})
+
+
 server.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 })
