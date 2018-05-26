9650b03b1364993ae05965dfe393f0fa4202ba2f
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2018-05-26 16:49:10 +0530

    Refactored the code to segregate all the student routes in a seperate file to define all the routes. Plug in the routes as a express middleware.

--- app.js
+++ app.js
@@ -1,53 +1,20 @@
 import express from 'express';
-import students from './data/students.json';
+
 import _ from 'lodash';
+import studentRoute from './routes/studentroute';
 
 const PORT = 3000;
 const server = express();
 const buildUrl = (version, path) => `/api/${version}/${path}`
 const STUDENT_BASE_URL = buildUrl('v1', 'students');
 
+
+server.use(STUDENT_BASE_URL,studentRoute);
+
 server.get('/', (req, res) => {
   res.send('My first route using express js');
 });
 
-
-server.get(`${STUDENT_BASE_URL}/:id`, (req, res) => {
-  const userId = req.params.id;
-  const studentf = students.filter((data) => data.id === userId);
-  if (studentf) {
-    res.json(studentf);
-  }
-  else {
-    res.send(`User ${userId} is not found`);
-  }
-  //res.end();
-});
-
-
-server.get(STUDENT_BASE_URL, (req, res) => {
-  res.json(students);
-});
-
-
-server.post(STUDENT_BASE_URL, (req, res) => {
-  console.log("handling POST request");
-  res.end();
-  //res.json(students);
-});
-
-
-server.put(STUDENT_BASE_URL, (req, res) => {
-  console.log("handling PUT request");
-  res.end();
-  //res.json(students);
-});
-
-server.delete(STUDENT_BASE_URL, (req, res) => {
-  console.log("handling DELETE request");
-  res.end();
-  //res.json(students);
-});
 
 
 server.get('/route-handlers', (req,res, next) => {
