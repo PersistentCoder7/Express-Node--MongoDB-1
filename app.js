31edae9ad2736cc09cd3d82f3f188df432828eb5
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2018-05-26 15:12:25 +0530

    Added a new route to view students data.

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import students from './data/students.json';
 
 const PORT = 3000;
 const server = express();
+const buildUrl = (version,path) => `/api/${version}/${path}`
 
 server.get('/',(req,res) => {
 res.send('My first route using express js');
-})
+});
 
+server.get(buildUrl('v1','students'),(req,res) => {
+  res.json(students);
+  });
+  
 
 server.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
-})
+});
